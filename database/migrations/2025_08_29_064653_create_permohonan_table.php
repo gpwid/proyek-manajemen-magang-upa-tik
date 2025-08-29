1e8d165d893de01a64ae6d95128f28456b341355
gustcode <gustipanji2006@gmail.com>
2025-08-29 07:37:50 +0000

    Merge model & controller

---
+++ database/migrations/2025_08_29_064653_create_permohonan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permohonan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_instansi');
+            $table->date('tgl_surat');
+            $table->date('instansi');
+            $table->date('tgl_mulai');
+            $table->date('tgl_selesai');
+            $table->string('pembimbing_sekolah', 255);
+            $table->string('kontak_pembimbing', 13);
+            $table->date('tgl_suratmasuk');
+            $table->enum('jenis_magang', ['Mandiri', 'MBKM', 'Sekolah']);
+            $table->enum('status', ['Aktif', 'Pending', 'Selesai'])->default('Pending');
+            $table->file('file_permohonan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permohonan');
+    }
+};
