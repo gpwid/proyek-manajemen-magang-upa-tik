221678bd1ebfde623602f9b0c6c503f679a6499e
Gusti Panji Widodo <gustipanji2006@gmail.com>
2025-09-18 22:46:50 +0700

    Bug fix

--- app/Http/Controllers/Admin/InternshipController.php
+++ app/Http/Controllers/Admin/InternshipController.php
@@ -83,7 +83,6 @@ class InternshipController extends Controller
             ->filter(function ($query) use ($request) {
                 if ($request->has('search') && $request->search['value'] != '') {
                     $keyword = $request->search['value'];
-                    // Grouping kondisi pencarian dengan where() closure
                     $query->where(function ($q) use ($keyword) {
                         $q->where('id', 'like', "%{$keyword}%")
                             ->orWhere('status_magang', 'like', "%{$keyword}%")
@@ -97,7 +96,7 @@ class InternshipController extends Controller
                             });
                     });
                 }
-            }, true)
+            })
             ->rawColumns(['status_magang', 'aksi'])
             ->make(true);
     }
