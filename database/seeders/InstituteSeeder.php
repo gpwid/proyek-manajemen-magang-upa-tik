f6c374cfb123708470c0c76aa74691fa8c81fd3a
zhidansaaba <zhidan.iannov4924@student.unri.ac.id>
2025-09-19 14:18:08 +0700

    Institute seeder update

--- database/seeders/InstituteSeeder.php
+++ database/seeders/InstituteSeeder.php
@@ -1,5 +1,5 @@
 <?php
-// database/seeders/InstituteSeeder.php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -9,8 +9,24 @@ class InstituteSeeder extends Seeder
 {
     public function run(): void
     {
-        // contoh pakai factory
-        Institute::factory()->count(5)->create();
+        $institusi = [
+            ['nama_instansi' => 'Universitas Riau (UNRI)',              'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'UIN Sultan Syarif Kasim Riau (UIN SUSKA)','alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'Universitas Islam Riau (UIR)',         'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'Universitas Lancang Kuning (UNILAK)',  'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'Universitas Muhammadiyah Riau (UMRI)', 'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'Politeknik Caltex Riau (PCR)',         'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'SMAN 1 Pekanbaru',                     'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'SMAN 5 Pekanbaru',                     'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'SMKN 1 Pekanbaru',                     'alamat' => 'Pekanbaru, Riau'],
+            ['nama_instansi' => 'SMK Hasanah Pekanbaru',                'alamat' => 'Pekanbaru, Riau'],
+        ];
+
+        foreach ($institusi as $i) {
+            Institute::updateOrCreate(
+                ['nama_instansi' => $i['nama_instansi']],
+                ['alamat' => $i['alamat']]
+            );
+        }
     }
 }
-
