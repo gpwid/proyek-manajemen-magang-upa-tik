13f4ceb5017bd7f4a372ac485408bc93c8f90e9c
zhidaniannov <zhidan.iannov4924@student.unri.ac.id>
2025-10-03 15:46:10 +0700

    AKun Pembimbing

---
+++ storage/framework/views/2044fccc1320327c7af0f129c813e232.php
@@ -0,0 +1,70 @@
+<?php $__env->startSection('title', 'Tambah Instansi'); ?>
+<?php $__env->startSection('instansi-active', 'active'); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-2">
+        <h1 class="h3 mb-3 text-gray-800">Form Tambah Instansi</h1>
+    </div>
+
+    <div class="card shadow col-12 p-4">
+        <form action="<?php echo e(route('admin.instansi.store')); ?>" method="POST">
+            <?php echo csrf_field(); ?>
+
+            
+            <div class="mb-3">
+                <label class="form-label">Nama Instansi<span class="text-danger">*</span></label>
+                <input value="<?php echo e(old('nama_instansi')); ?>" type="text" name="nama_instansi"
+                       class="form-control <?php $__errorArgs = ['nama_instansi'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                       placeholder="Masukkan nama instansi">
+                <?php $__errorArgs = ['nama_instansi'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> <div class="invalid-feedback"><?php echo e($message); ?></div> <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+
+            
+            <div class="mb-3">
+                <label class="form-label">Alamat <span class="text-danger">*</span></label>
+                <input value="<?php echo e(old('alamat')); ?>" type="text" name="alamat"
+                       class="form-control <?php $__errorArgs = ['alamat'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                       placeholder="Masukkan alamat">
+                <?php $__errorArgs = ['alamat'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> <div class="invalid-feedback"><?php echo e($message); ?></div> <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+
+            
+            <div class="d-flex justify-content-end">
+                <a class="btn btn-secondary mr-2" href="<?php echo e(route('admin.instansi.index')); ?>">Kembali</a>
+                <button type="submit" class="btn btn-primary">Tambah</button>
+            </div>
+        </form>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('admin.layoutsadmin.main', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\magang\resources\views/admin/instansi/create.blade.php ENDPATH**/ ?>
